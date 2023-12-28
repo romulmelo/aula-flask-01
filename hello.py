0abe3b9b243227c3cc12505fccbb27672a4008f3
Fabio Teixeira <53839583+fabio-teixeira-ifsp@users.noreply.github.com>
2023-12-28 14:47:18 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return '<p>Hello from Flask!</p><table><tr><td><b>Aluno:</b></td><td>Fabio Teixeira</td></tr><tr><td><b>Prontuário</b></td><td>PT23820X</td></tr></table>'
+    return '<p>Hello from Flask!</p><table><tr><td><b>Aluno:</b></td><td>Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
